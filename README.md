a6a3e36958491d6410eb13e60729e22de594aa0c
alexhaynes32 <alhaynes@seas.upenn.edu>
2017-08-12 20:40:31 -0700

    working

--- README.md
+++ README.md
@@ -5,7 +5,6 @@ Teaching computers to have fun
 1. Download [Nintaco](http://nintaco.com/index.html) save the zip contents to `nintaco/`
 2. Download [Nintaco SDK](http://nintaco.com/api.html) save the zip contents to `nintaco_API/`
 3. Get your hands on a Super Mario Bros. ROM, and save it in `roms/`
-4. run `nintaco.jar`
-5. do `file > open ` and select the ROM.
+4. run `java -jar nintaco/Nintaco.jar roms/rom_name.nes` where `rom_name.nes` is the name of your rom
 6. do `tools > start program server` then in the pop up click `start server`
 7. Run `python test.py`
--- test.py
+++ test.py
@@ -1,79 +1,112 @@
 import sys
+import math
 from nintaco_API import nintaco
 
-STRING = "Hello, World!"
-  
+STRING = "FITNESS: 000000"
+WALKED = "FITNESS: "
+STRING_1 = "Gen: 1 Mutation: A"
+WALKED_VAL = 0
+	
 SPRITE_ID = 123
 SPRITE_SIZE = 32
-  
-spriteX = 0
-spriteY = 8
-spriteVx = 1
-spriteVy = 1
+	
+marioX = 0
+marioY = 0
+screenX = 0
+screenY = 0
 
 nintaco.initRemoteAPI("localhost", 9999)
 api = nintaco.getAPI()
-  
+
+def getPositions():
+	marioX = api.readCPU(0x6D) * 0x100 + api.readCPU(0x86)
+	marioY = api.readCPU(0x03B8) + 16
+
+	screenX = api.readCPU(0x03AD)
+	screenY = api.readCPU(0x03B8)
+
+def getTile(dx, dy):
+	x = marioX + dx + 8
+	y = marioY + dy - 16
+	page = math.floor(x / 256) % 2
+	subx = math.floor((x%256)/16)
+	suby = math.floor((y - 32)/16)
+	addr = 0x500 + page * 13 * 16 + suby * 16 + subx
+	if suby >= 13 or suby < 0:
+		return 0
+	if api.readCPU(addr) != 0:
+		return 1
+	else:
+		return 0
+
+def getSprites():
+	sprites = {}
+	for slot in range(0,5):
+		enemy = api.readCPU(0xF + slot)
+		if enemy != 0:
+			ex = api.readCPU(0x6E + slot) * 0x100 + api.readCPU(0x87 + slot)
+			ey = api.readCPU(0xCF + slot) + 24
+			sprites[len(sprites) + 1] = {"x": ex, "y": ey}
+	return sprites
+
+
+	
 def launch():
-  api.addFrameListener(renderFinished)
-  api.addStatusListener(statusChanged)
-  api.addActivateListener(apiEnabled)
-  api.addDeactivateListener(apiDisabled)
-  api.addStopListener(dispose)
-  api.run()
-  
+	api.addFrameListener(renderFinished)
+	api.addStatusListener(statusChanged)
+	api.addActivateListener(apiEnabled)
+	api.addDeactivateListener(apiDisabled)
+	api.addStopListener(dispose)
+	api.run()
+	
 def apiEnabled():
-  global strWidth
-  global strX
-  global strY
-  
-  print("API enabled")
+	global strWidth
+	global strWidth1
+	global strX
+	global strY
+	global strX1
+	global strY1
+	
+	print("API enabled")
 
-  sprite = [0 for i in range(SPRITE_SIZE * SPRITE_SIZE)]
-  for y in range(SPRITE_SIZE):
-    Y = y - SPRITE_SIZE / 2 + 0.5
-    for x in range(SPRITE_SIZE):
-      X = x - SPRITE_SIZE / 2 + 0.5
-      sprite[SPRITE_SIZE * y + x] = nintaco.ORANGE if (X * X + Y * Y 
-          <= SPRITE_SIZE * SPRITE_SIZE / 4) else -1
-  api.createSprite(SPRITE_ID, SPRITE_SIZE, SPRITE_SIZE, sprite)
-  strWidth = api.getStringWidth(STRING, False)
-  strX = (256 - strWidth) / 2
-  strY = (240 - 8) / 2
-  
+	sprite = [0 for i in range(SPRITE_SIZE * SPRITE_SIZE)]
+	for y in range(SPRITE_SIZE):
+		Y = y - SPRITE_SIZE / 2 + 0.5
+		for x in range(SPRITE_SIZE):
+			X = x - SPRITE_SIZE / 2 + 0.5
+			sprite[SPRITE_SIZE * y + x] = nintaco.ORANGE if (X * X + Y * Y 
+					<= SPRITE_SIZE * SPRITE_SIZE / 4) else -1
+	api.createSprite(SPRITE_ID, SPRITE_SIZE, SPRITE_SIZE, sprite)
+	strWidth = api.getStringWidth(STRING, False)
+	strWidth1 = api.getStringWidth(STRING_1, False)
+	strX = (256 - strWidth) / 2 - 50
+	strY = 10
+	strX1 = (256 - strWidth) / 2 + 50
+	strY1 = 10
+	
 def apiDisabled():
-  print("API disabled")
-  
+	print("API disabled")
+	
 def dispose():
-  print("API stopped")
-  
+	print("API stopped")
+	
 def statusChanged(message):
-  print("Status message: %s" % message)
-  
+	print("Status message: %s" % message)
+	
 def renderFinished():
-  global spriteX
-  global spriteY
-  global spriteVx
-  global spriteVy
-
-  api.drawSprite(SPRITE_ID, spriteX, spriteY)
-  if spriteX + SPRITE_SIZE == 255:
-    spriteVx = -1
-  elif spriteX == 0:
-    spriteVx = 1
-  if spriteY + SPRITE_SIZE == 231:
-    spriteVy = -1
-  elif spriteY == 8:
-    spriteVy = 1
-  spriteX += spriteVx
-  spriteY += spriteVy
-
-  api.setColor(nintaco.DARK_BLUE)
-  api.fillRect(strX - 1, strY - 1, strWidth + 2, 9)
-  api.setColor(nintaco.BLUE)
-  api.drawRect(strX - 2, strY - 2, strWidth + 3, 10)
-  api.setColor(nintaco.WHITE)
-  api.drawString(STRING, strX, strY, False)
-  
+	WALKED_VAL = api.readCPU(0x6D) * 0x100 + api.readCPU(0x86)
+	STRING = WALKED + str(WALKED_VAL)
+	api.setColor(nintaco.DARK_BLUE)
+	api.fillRect(strX - 1, strY - 1, strWidth + 2, 9)
+	api.setColor(nintaco.BLUE)
+	api.drawRect(strX - 2, strY - 2, strWidth + 3, 10)
+	api.setColor(nintaco.DARK_BLUE)
+	api.fillRect(strX1 - 1, strY1 - 1, strWidth1 + 2, 9)
+	api.setColor(nintaco.BLUE)
+	api.drawRect(strX1 - 2, strY1 - 2, strWidth1 + 3, 10)
+	api.setColor(nintaco.WHITE)
+	api.drawString(STRING, strX, strY, False)
+	api.drawString(STRING_1, strX1, strY1, False)
+	
 if __name__ == "__main__":
-  launch()
+	launch()
